61e3525542531dd4c7d6ce6e305f39ef77f4e49f
John Biddulph <jbiddulph@bluecresthealth.com>
2020-02-26 21:44:57 +0000

    got users data in an angular material table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './users.service';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = 'dynamic-material-table';
-  users: Array<object>;
+export class AppComponent implements OnInit {
   constructor(private usersService: UsersService) { }
-
+  name: string;
+  gender: string;
+  company: string;
+  users: Array<object>;
+  usersList: any[] = this.usersService.get();
+  displayedColumns: string[] = ['name', 'gender', 'company'];
+  dataSource = this.usersList;
   ngOnInit() {
     this.usersService.get().subscribe(res => {
       this.users = res;
     });
   }
 }
+
+
