049eb3242667b76106916d1d13e28d4f7e84fd3c
John Biddulph <jbiddulph@bluecresthealth.com>
2020-02-26 21:16:09 +0000

    getting users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dynamic-material-table';
+  users: Array<object>;
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit() {
+    this.usersService.get().subscribe(res => {
+      this.users = res;
+    });
+  }
 }
